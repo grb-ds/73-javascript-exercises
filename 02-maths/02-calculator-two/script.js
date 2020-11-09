01eda5807443c0820fa330fbba387834ba1a7ef5
Giomayra Rosales <giomayra.rosales@gmail.com>
2020-11-09 16:45:51 +0100

    commit maths-02

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -13,7 +13,52 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     const performOperation = operation => {
-        // perform the operation
+        // perform the
+        let value1, value2, total, txt;
+
+        value1 = parseFloat(document.getElementById("op-one").value);
+        value2 = parseFloat(document.getElementById("op-two").value);
+
+        console.log("Value1: " + value1);
+        console.log("Value2: " + value2);
+        console.log(operation);
+        switch (operation){
+            case "addition":
+                total = value1 + value2;
+                txt = "The addition result is: " + parseFloat(total);
+                alert(txt);
+                document.getElementById("additionResult").innerHTML = txt;
+                console.log(txt);
+                break;
+            case "substraction":
+                total = value1 - value2;
+                txt = "The substraction result is: " + parseFloat(total);
+                alert(txt);
+                document.getElementById("substractionResult").innerHTML = txt;
+                console.log(txt);
+                break;
+            case "multiplication":
+                total = value1 * value2;
+                txt = "The multiplication result is: " + parseFloat(total);
+                alert(txt);
+                document.getElementById("multiplicationResult").innerHTML = txt;
+                console.log(txt);
+                break;
+            case "division":
+                if (value2 != 0) {
+                    total = value1 / value2;
+                    txt = "The division result is: " + parseFloat(total);
+                    alert(txt);
+                    document.getElementById("divisionResult").innerHTML = txt;
+                    console.log(txt);
+                } else{
+                    txt = "Divide by zero Error!";
+                    alert(txt)
+                    document.getElementById("divisionResult").innerHTML = txt;
+                    console.log(txt);
+                }
+                break;
+        }
     };
 
     Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
