2beae19fd321bd0dff3baf4ed38ed2b00f9cdd80
Giomayra Rosales <giomayra.rosales@gmail.com>
2020-11-10 16:25:59 +0100

    commit change bcg -three

--- 03-colors/03-change-bcg-three/script.js
+++ 03-colors/03-change-bcg-three/script.js
@@ -12,5 +12,10 @@
 (function() {
 
     // your code here
+    document.getElementById("run").addEventListener("click", function (){
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = "#" + randomColor;
+        document.getElementById("result").innerHTML = "#" + randomColor;
+    });
 
 })();
