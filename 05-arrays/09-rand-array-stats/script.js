15c211b4b149fd1889cdc2d9fa2f8c0337edfc8c
Giomayra Rosales <giomayra.rosales@gmail.com>
2020-11-13 04:27:50 +0100

    commit arrays 9

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -13,4 +13,35 @@
 
     // your code here
 
+    alert("Rand array");
+
+    document.getElementById("run").addEventListener("click", function (){
+
+        const number_table = document.getElementById('tb_numbers');
+        let random_number;
+        let random_array = new Array();
+
+       //let cellsOfRow = number_table.rows[0].getElementsByTagName('td');
+
+        Array.from(number_table.rows).forEach((row, row_ind) => {
+            Array.from(row.cells).forEach((cell,index) =>{
+                random_number = Math.floor(Math.random() * 100);
+                random_array.push(random_number);
+                cell.innerHTML = random_number;
+            });
+
+            let min = Math.min.apply(null, random_array);
+            let max = Math.max.apply(null, random_array);
+            let addition = random_array.reduce((a, b) => a + b, 0);
+            let average = addition/random_array.length;
+
+            document.getElementById("min").innerHTML = min.toString();
+            document.getElementById("max").innerHTML = max.toString();
+            document.getElementById("sum").innerHTML = addition.toString();
+            document.getElementById("average").innerHTML = average.toString();
+
+        });
+
+    });
+
 })();
