06170699d1db3209cf4dc35c7cdda95185b975d6
Giomayra Rosales <giomayra.rosales@gmail.com>
2020-11-15 16:18:27 +0100

    update Objects 3

--- 06-objects/03-keys-values/script.js
+++ 06-objects/03-keys-values/script.js
@@ -9,6 +9,8 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+alert("Objects 3");
+
 (() => {
     const person = {
         lastname: "Delnatte",
@@ -19,4 +21,28 @@
         status: "married",
     };
     // your code here
+
+    let personDescriptorsKey = Object.getOwnPropertyDescriptors(person);
+
+    document.getElementById("run").addEventListener("click", function (){
+
+        let index = 0;
+
+        Object.entries(person).forEach(([key, val]) => {
+            index++;
+            console.log(`Key ${index} : ${key} `);
+            document.getElementById("target1").innerHTML += `Key ${index} : ${key} <br/>`
+        });
+
+        index = 0;
+        Object.entries(person).forEach(([key, val]) => {
+            index++;
+            console.log(`Value [${key}] : ${val} `);
+            document.getElementById("target2").innerHTML += `Value [${key}] : ${val} <br/>`
+        });
+
+
+    });
+
+
 })();
