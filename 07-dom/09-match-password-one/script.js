7eccdd216198c95788ba4c309605dc343dd16e1f
Giomayra Rosales <giomayra.rosales@gmail.com>
2020-11-24 17:06:34 +0100

    update script 07-dom 09 exercise

--- 07-dom/09-match-password-one/script.js
+++ 07-dom/09-match-password-one/script.js
@@ -13,4 +13,38 @@
 
     // your code here
 
+/*    <input type="text" name="pass-one" id="pass-one" />
+</div>
+<div class="field">
+<label for="pass-two">Re-enter password:</label>
+<input type="text" name="pass-two" id="pass-two" />
+</div>
+<div class="actions">
+<button type="button" id="run">Run the script</button>*/
+
+
+
+    document.querySelector("#pass-one").setAttribute("type", "password");
+    document.querySelector("#pass-two").setAttribute("type", "password");
+
+    const passwordValidate = () => {
+        return function(){
+            let passwordNode1, passwordNode2;
+            passwordNode1 = document.querySelector("#pass-one");
+            passwordNode2 = document.querySelector("#pass-two");
+            console.log(passwordNode1.value);
+            console.log(passwordNode2.value);
+
+            if (passwordNode1.value === passwordNode2.value){
+                passwordNode1.style.borderColor = "green";
+                passwordNode2.style.borderColor = "green";
+            } else {
+                passwordNode1.style.borderColor = "red";
+                passwordNode2.style.borderColor = "red";
+            }
+        };
+    };
+
+    document.getElementById("run").addEventListener("click",passwordValidate());
+
 })();
