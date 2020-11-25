04219cb39979a1c02e3ac333d680a79e2e04380b
Giomayra Rosales <giomayra.rosales@gmail.com>
2020-11-25 15:11:08 +0100

    update script 07-dom 12 exercise and finish

--- 07-dom/12-change-event-input-two/script.js
+++ 07-dom/12-change-event-input-two/script.js
@@ -12,5 +12,34 @@
 (function() {
 
     // your code here
+    var minLength = 8;
+
+    document.querySelector("#pass-one").minLength = minLength;
+
+    /*
+    Credits URL:
+    https://stackoverflow.com/questions/1072765/count-number-of-matches-of-a-regex-in-javascript?answertab=votes#tab-top
+    this function return the ocurrens total of the 0-9 regular expression
+    */
+    const count = (str) => {
+        const re = /[0-9]/g
+        return ((str || '').match(re) || []).length
+    }
+
+    function validate(e) {
+        let message
+        let inputString = e.target.value;
+        console.log("inputString: " + inputString);
+        console.log("inputString.length: " + inputString);
+        let totalNumbers = count(inputString);
+        console.log(totalNumbers);
+
+        if (inputString.length >= minLength && totalNumbers >= 2)
+            document.getElementById("validity").innerHTML = "Ok";
+        else
+            document.getElementById("validity").innerHTML = "Not ok";
+    }
+
+    document.querySelector("#pass-one").addEventListener('input', validate);
 
 })();
